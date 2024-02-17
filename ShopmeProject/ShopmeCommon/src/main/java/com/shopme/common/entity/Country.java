b298f705ab765bd3b6d52ca992e0760dc4bd56fd
sanykhan <usmanshj3@gmail.com>
2024-02-17 21:27:20 +0500

    Implemented backend and frontend functionalities for managing countries, states/provinces, and customer registration, improving Shopme's shipping and user registration.

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
@@ -30,18 +30,26 @@ public class Country {
 		
 	}
 	
+	public Country(Integer id) {
+		this.id = id;
+	}
+
 	public Country(Integer id, String name, String code) {
-		
 		this.id = id;
 		this.name = name;
 		this.code = code;
 	}
+
 
 	public Country(String name, String code) {
 		this.name = name;
 		this.code = code;
 	}
 
+	public Country(String name) {
+		this.name = name;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -64,14 +72,6 @@ public class Country {
 
 	public void setCode(String code) {
 		this.code = code;
-	}
-
-	public Set<State> getStates() {
-		return states;
-	}
-
-	public void setStates(Set<State> states) {
-		this.states = states;
 	}
 
 	@Override
