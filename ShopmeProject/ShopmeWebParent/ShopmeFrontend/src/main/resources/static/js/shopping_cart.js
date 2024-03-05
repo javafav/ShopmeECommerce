e3283b8577b918dea6957b7c7da013d1606faad8
sanykhan <usmanshj3@gmail.com>
2024-03-05 21:51:35 +0500

    Completed the shipping rate module for the admin panel. Additionally, enhanced the shopping cart functionality to display a message when the customer removes all products, ensuring better user experience.

--- ShopmeProject/ShopmeWebParent/ShopmeFrontend/src/main/resources/static/js/shopping_cart.js
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontend/src/main/resources/static/js/shopping_cart.js
@@ -1,27 +1,31 @@
+
+decimalSeparator = decimalPointType == 'COMMA' ? ',' : '.';
+thousandsSeparator = thousandsPointType == 'COMMA' ? ',' : '.';
+
 $(document).ready(function() {
-	
+
 	$(".linkMinus").on("click", function(evt) {
 		evt.preventDefault();
-		
+
 		decreaseQuantity($(this));
-		
+
 	});
-	
+
 	$(".linkPlus").on("click", function(evt) {
 		evt.preventDefault();
-		
+
 		increaseQuantity($(this));
-		
-	});	
-	
+
+	});
+
 	$(".linkRemove").on("click", function(evt) {
-		
+
 		evt.preventDefault();
-		
+
 		removeProduct($(this));
-		
-	
-		
+
+
+
 	});
 });
 
@@ -29,35 +33,35 @@ function decreaseQuantity(link) {
 	productId = link.attr("pid");
 	quantityInput = $("#quantity" + productId);
 	newQuantity = parseInt(quantityInput.val()) - 1;
-	
+
 	if (newQuantity > 0) {
-	
+
 		quantityInput.val(newQuantity);
 		updateQuantity(productId, newQuantity);
-		
+
 	} else {
 		showWarningModal('Minimum quantity is 1');
-	}	
+	}
 }
 
 function increaseQuantity(link) {
-		productId = link.attr("pid");
-		quantityInput = $("#quantity" + productId);
-		newQuantity = parseInt(quantityInput.val()) + 1;
-		
-		if (newQuantity <= 5) {
-			
-			quantityInput.val(newQuantity);
-			updateQuantity(productId, newQuantity);
-			
-		} else {
-			showWarningModal('Maximum quantity is 5');
-		}	
+	productId = link.attr("pid");
+	quantityInput = $("#quantity" + productId);
+	newQuantity = parseInt(quantityInput.val()) + 1;
+
+	if (newQuantity <= 5) {
+
+		quantityInput.val(newQuantity);
+		updateQuantity(productId, newQuantity);
+
+	} else {
+		showWarningModal('Maximum quantity is 5');
+	}
 }
 
 function updateQuantity(productId, quantity) {
 	url = contextPath + "cart/update/" + productId + "/" + quantity;
-	
+
 	$.ajax({
 		type: "POST",
 		url: url,
@@ -70,23 +74,31 @@ function updateQuantity(productId, quantity) {
 		updateTotal();
 	}).fail(function() {
 		showErrorModal("Error while updating product quantity.");
-	});	
+	});
 }
 
 function updateSubtotal(updatedSubtotal, productId) {
-	formattedSubtotal = $.number(updatedSubtotal, 2);
-	$("#subtotal" + productId).text(formattedSubtotal);
+
+	$("#subtotal" + productId).text(formatCurrency(updatedSubtotal));
 }
 
 function updateTotal() {
 	total = 0.0;
-	
+	productCount = 0;
+
 	$(".subtotal").each(function(index, element) {
-		total += parseFloat(element.innerHTML.replaceAll(",", ""));
+		productCount++;
+
+		total += parseFloat(clearCurrencyFormat(element.innerHTML));
+
+
 	});
-	
-	formattedTotal = $.number(total, 2);
-	$("#total").text(formattedTotal);
+	if (productCount < 1) {
+		showEmptyShoppingCart();
+	} else {
+		$("#total").text(formatCurrency(total));
+	}
+	$("#total").text(formatCurrency(total));
 }
 
 function removeProduct(link) {
@@ -99,19 +111,19 @@ function removeProduct(link) {
 			xhr.setRequestHeader(csrfHeaderName, csrfValue);
 		}
 	}).done(function(responseJSON) {
-	 rowNumber = link.attr("rowNumber")
-	 removeProductHTML(rowNumber);
-	 updateTotal();
-	 updateCountNumber();
-	 totalCartItem();
-	showModalDialog("Shopping Cart", responseJSON);
+		rowNumber = link.attr("rowNumber")
+		removeProductHTML(rowNumber);
+		updateTotal();
+		updateCountNumbers();
+		totalCartItem();
+		showModalDialog("Shopping Cart", responseJSON);
 	}).fail(function() {
 		showErrorModal("Error while removing product .");
 	});
 
 }
 
-function removeProductHTML(rowNumber){
+function removeProductHTML(rowNumber) {
 	$("#row" + rowNumber).remove();
 }
 function removeProductHTML(rowNumber) {
@@ -122,5 +134,18 @@ function removeProductHTML(rowNumber) {
 function updateCountNumbers() {
 	$(".divCount").each(function(index, element) {
 		element.innerHTML = "" + (index + 1);
-	}); 
+	});
+}
+
+function formatCurrency(amount) {
+	return $.number(amount, decimalDigits, decimalSeparator, thousandsSeparator);
+}
+function clearCurrencyFormat(numberString) {
+	result = numberString.replaceAll(thousandsSeparator, "");
+	return result.replaceAll(decimalSeparator, ".");
+}
+
+function showEmptyShoppingCart() {
+	$("#sectionTotal").hide();
+	$("#sectionEmptyCartMessage").removeClass("d-none");
 }
