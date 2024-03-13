155c69d434149f5d2bf64af28861ac078d8d2bcb
sanykhan <usmanshj3@gmail.com>
2024-03-13 15:52:45 +0500

    Completed the order detail modal dialog functionality.

--- ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/order/OrderController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/order/OrderController.java
@@ -6,13 +6,17 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.paging.PagingAndSortingHelper;
 import com.shopme.admin.paging.PagingAndSortingParam;
 import com.shopme.admin.setting.SettingService;
+import com.shopme.common.entity.Order;
 import com.shopme.common.entity.Setting;
+import com.shopme.common.exception.OrderNotFoundException;
 
 @Controller
 public class OrderController {
@@ -36,6 +40,22 @@ public class OrderController {
 		loadCurrencySetting(request);
 		
 		return "orders/orders";
+	}
+	
+	@GetMapping("/orders/detail/{id}")
+	public String viewOrderDetails(@PathVariable("id") Integer id, Model model, 
+			RedirectAttributes ra, HttpServletRequest request) {
+		try {
+			Order order = orderService.get(id);
+			loadCurrencySetting(request);			
+			model.addAttribute("order", order);
+			
+			return "orders/order_details_modal";
+		} catch (OrderNotFoundException ex) {
+			ra.addFlashAttribute("message", ex.getMessage());
+			return defaultRedirectURL;
+		}
+		
 	}
 	
 	private void loadCurrencySetting(HttpServletRequest request) {
