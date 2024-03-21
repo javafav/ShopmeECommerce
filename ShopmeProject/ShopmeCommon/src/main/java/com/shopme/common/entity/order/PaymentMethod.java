2be5e7f1ef2dc4897b353e22d6b7c47fe1b7ba76
sanykhan <usmanshj3@gmail.com>
2024-03-21 21:22:27 +0500

    Completed PayPal integration and implemented entity classes with test methods for order tracking.

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/order/PaymentMethod.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/order/PaymentMethod.java
@@ -1,5 +1,5 @@
 package com.shopme.common.entity.order;
 
 public enum PaymentMethod {
-	COD, CREDIT_CARD, DEBIT_CARD
+	COD, CREDIT_CARD, DEBIT_CARD, PAYPAL
 }
--- ShopmeProject/ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/checkout/CheckoutController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/checkout/CheckoutController.java
@@ -14,11 +14,14 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shopme.Utility;
 import com.shopme.address.AddressService;
+import com.shopme.checkout.paypal.PayPalApiException;
+import com.shopme.checkout.paypal.PayPalService;
 import com.shopme.common.entity.Address;
 import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
@@ -35,6 +38,7 @@ import com.shopme.shipping.ShippingRateService;
 import com.shopme.shoppingcart.ShoppingCartService;
 
 @Controller
+@CrossOrigin()
 public class CheckoutController {
 
 	@Autowired private CheckoutService checkoutService;
@@ -44,6 +48,7 @@ public class CheckoutController {
 	@Autowired private ShoppingCartService cartService;
 	@Autowired private OrderService orderService;
 	@Autowired private SettingService settingService;
+	@Autowired private PayPalService paypalService;
 	
 	@GetMapping("/checkout")
 	public String showCheckoutPage(Model model, HttpServletRequest request) {
@@ -148,5 +153,31 @@ public class CheckoutController {
 		
 		helper.setText(content, true);
 		mailSender.send(message);		
+	}
+	
+	@PostMapping("/process_paypal_order")
+	public String processPayPalOrder(HttpServletRequest request, Model model) 
+			throws UnsupportedEncodingException, MessagingException {
+		String orderId = request.getParameter("orderId");
+		
+		String pageTitle = "Checkout Failure";
+		String message = null;
+		
+		try {
+			if (paypalService.validateOrder(orderId)) {
+				return placeOrder(request);
+			} else {
+				pageTitle = "Checkout Failure";
+				message = "ERROR: Transaction could not be completed because order information is invalid";
+			}
+		} catch (PayPalApiException e) {
+			message = "ERROR: Transaction failed due to error: " + e.getMessage();
+		}
+		
+		model.addAttribute("pageTitle", pageTitle);
+		model.addAttribute("title", pageTitle);
+		model.addAttribute("message", message);
+		
+		return "message";
 	}
 }
