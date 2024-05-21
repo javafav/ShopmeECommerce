3f54839330bdaad145e67ef9d40d9ce81b8b395f
sanykhan <usmanshj3@gmail.com>
2024-05-21 22:11:47 +0500

    Update order page layout and modals for Bootstrap 5.3.2

--- ShopmeProject/ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/security/WebSecurityConfig.java
+++ ShopmeProject/ShopmeWebParent/ShopmeFrontend/src/main/java/com/shopme/security/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.antMatchers("/account_details", "/update_account_details", "/orders/**",
 				"/cart", "/address_book/**", "/checkout", "/place_order", "/review/**", 
-				"/process_paypal_order","/write_review/**", "/post_review").authenticated()
+				"/process_paypal_order","/write_review/**", "/post_review","/customer/questions/**").authenticated()
 		.anyRequest().permitAll()
 		.and()
 		.formLogin()
